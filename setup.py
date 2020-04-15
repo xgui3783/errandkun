9030cd39c42114da21a5f0610e28ca19bce9a239
Xiao Gui <xgui3783@gmail.com>
2020-04-15 11:40:49 +0200

    chore: update packages

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name='errandkun',
   version='0.0.2',
-  packages=['errandkun'],
+  packages=find_packages(),
   license='MIT',
   entry_points={
     'mkdocs.plugins': [
